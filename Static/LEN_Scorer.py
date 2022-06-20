5d496fc54bff51be8c64af37f347f8e1da446eb2
Benjamin Frost <43476782+BenMarkFrost@users.noreply.github.com>
2022-06-20 21:14:35 +0100

    Updating scorer

--- Static/LEN_Scorer.py
+++ Static/LEN_Scorer.py
@@ -6,37 +6,29 @@ from torch_explain.nn.functional import l1_loss
 from torch_explain.logic.nn import psi
 from torch_explain.logic.metrics import test_explanation , complexity
 from torch_explain.logic.nn import entropy
+import numpy as np
 
 class Scorer:
 
     """Runs categorised static data on a standard LEN network, and outputs the result."""
 
-    def __init__(self, file_name):
-        self.score_file = self.load_scores(file_name)
+    def __init__(self, data, target):
+        self.data = data
+        self.target = target
         
-        self.score()
-        
-    
-    def load_scores(self, file_name):
-        return pd.read_csv(file_name)
 
-    self.score(self):
-        x0 = torch.zeros((4, 100))
-        x_train = torch.tensor([
-            [0, 0],
-            [0, 1],
-            [1, 0],
-            [1, 1],
-        ], dtype=torch.float)
-        x_train = torch.cat([x_train, x0], dim=1)
-        y_train = torch.tensor([1, 0, 0, 1], dtype=torch.long)
+    def score(self):
+        x_train = torch.tensor(self.data.to_numpy(), dtype=torch.float)
+        y_train = torch.tensor(self.target.to_numpy(), dtype=torch.long)
+
+        print(y_train)
 
         layers = [
-            te.nn.EntropyLinear(x_train.shape[1], 10, n_classes=2),
+            te.nn.EntropyLinear(x_train.shape[1], 10, n_classes=4),
             torch.nn.LeakyReLU(),
             torch.nn.Linear(10, 4),
             torch.nn.LeakyReLU(),
-            torch.nn.Linear(4, 1),
+            torch.nn.Linear(4, 4),
         ]
         model = torch.nn.Sequential(*layers)
 
@@ -44,11 +36,14 @@ class Scorer:
         optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
         loss_form = torch.nn.CrossEntropyLoss()
         model.train()
-        for epoch in range(1001):
+        for _ in range(1001):
             optimizer.zero_grad()
             y_pred = model(x_train).squeeze(-1)
+            # print((y_pred))
             loss = loss_form(y_pred, y_train)
-            print(y_pred, y_train)
             loss = loss_form(y_pred, y_train) + 0.00001 * te.nn.functional.entropy_logic_loss(model)
+            print(loss)
             loss.backward()
             optimizer.step()
+
+        print(y_pred, y_train)
