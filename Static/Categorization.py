fa224e68f87fbae09e5c33efc50de05b3847dbda
Benjamin Frost <43476782+BenMarkFrost@users.noreply.github.com>
2022-07-01 16:53:15 +0100

    Adding Categorization Class

---
+++ Static/Categorization.py
@@ -0,0 +1,112 @@
+from sklearn.preprocessing import KBinsDiscretizer
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Categorizer:
+
+    def __init__(self, data):
+        self.data = data
+        self.categorizationTypes = {}
+
+    def reorder(self, data, bins):
+
+        # This reorders the categories to the ascending order of the values in the original DF. Took bloody ages to figure this out.
+
+        for col in data:
+            # Getting the REVERSE index of the ordered values in the original DF. Double index thing took ages.
+            tempDF = pd.DataFrame()
+            # Sort by the values
+            tempDF['idx'] = list(self.data[col].sort_values().index)
+            # Then sort by the result's index
+            tempDF = tempDF.sort_values(by = 'idx')
+
+            # Using the created index to reorder the categories.
+            tempDF2 = data.copy()
+            tempDF2['idx'] = tempDF.index
+            tempDF2 = tempDF2.set_index('idx').sort_index()
+            # Mapping from previous categories to the new ordered categories.
+            mapping = dict(zip(tempDF2[col].unique(), list(range(bins))))
+            data[col] = data[col].map(mapping)
+
+        return data
+
+
+    def kBins(self, bins):
+
+        est = KBinsDiscretizer(n_bins=bins, encode='ordinal', strategy='uniform')
+        kBinsDF = pd.DataFrame(data=est.fit_transform(self.data), columns=self.data.columns)
+
+        self.categorizationTypes['kBins'] = kBinsDF
+
+        return kBinsDF
+
+    def kMeans(self, n_clusters):
+
+        kMeansDF = self.data.apply(lambda x: KMeans(n_clusters=n_clusters, random_state=0).fit_predict(np.asarray(x).reshape(-1,1)))
+
+        reorderedKMeansDF = self.reorder(kMeansDF, bins = n_clusters)
+
+        self.categorizationTypes['kMeans'] = reorderedKMeansDF
+
+        return kMeansDF
+    
+    def agglomerative(self, n_clusters):
+
+        agglomerativeDF = self.data.apply(lambda x: AgglomerativeClustering(n_clusters=n_clusters).fit_predict(np.asarray(x).reshape(-1,1)))
+
+        reorderedAgglomerativeDF = self.reorder(agglomerativeDF, bins = n_clusters)
+
+        self.categorizationTypes['agglomerative'] = reorderedAgglomerativeDF
+
+        return agglomerativeDF
+    
+    def display(self):
+
+        for type in self.categorizationTypes:
+
+            df = self.categorizationTypes[type]
+
+            if len(self.data.columns) < 6:
+
+                fig = plt.figure(figsize=(10,6), dpi=100)
+
+                fig.suptitle(f"{type}")
+
+                for idx, col in enumerate(self.data.columns):
+
+                    score = silhouette_score(np.asarray(self.data[col]).reshape(-1,1), df[col])
+
+                    plt.subplot(int(len(self.data.columns)/2), int(len(self.data.columns)/2), idx+1)
+                    plt.scatter(self.data[col], self.data[col].map(self.data[col].value_counts()), c=df[col])
+                    plt.xlabel("Value")
+                    plt.ylabel("Frequency")
+                    plt.title(f"{col}, score: {score}")
+            
+                plt.tight_layout()
+                plt.show()
+
+            else:
+
+                for idx, col in enumerate(self.data.columns):
+
+                    fig = plt.figure()
+
+                    fig.suptitle(f"{type}")
+
+                    score = silhouette_score(np.asarray(self.data[col]).reshape(-1,1), df[col])
+
+                    plt.scatter(self.data[col], self.data[col].map(self.data[col].value_counts()), c=df[col])
+                    plt.xlabel("Value")
+                    plt.ylabel("Frequency")
+                    plt.title(f"{col}, score: {score}")
+
+                    plt.tight_layout()
+                    plt.show()
+
+
+            
